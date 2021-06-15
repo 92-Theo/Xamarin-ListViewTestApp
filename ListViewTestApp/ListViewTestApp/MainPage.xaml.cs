daaf13185e120edc6c39c17cc2f526b6378e9241
CHOI TAEYOUNG <55708666+92-Theo@users.noreply.github.com>
2021-06-15 16:37:07 +0900

    Initial commit

---
+++ ListViewTestApp/ListViewTestApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewTestApp
+{
+    public class ListViewItem
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public int TabIndex { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var list = new List<ListViewItem>();
+            for (var i = 1; i < 10; i++)
+            {
+                list.Add(new ListViewItem() { Name = $"name{i}", Value = $"{i}", TabIndex = i });
+            }
+            _listView.ItemsSource = list;
+        }
+
+       
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ListViewTestApp
+ 
